c049aefdeb07ffd674e27b137aee380a2c882d14
Racetrack69 <christopher.schmidtke@outlook.com>
2026-09-18 10:44:55 +0200

    Add files via upload

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // Eine Stelle für die Versionsnummer. Wird von index.html (Fußzeile) und sw.js (Cache-Name) gelesen.
 // Bei jeder Änderung an den Dateien hochzählen, sonst behalten installierte Apps den alten Stand.
-self.APP_VERSION = 'v12';
+self.APP_VERSION = 'v13';
 self.APP_DATE = '18.09.2026';
